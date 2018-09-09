cc46939e485261796fd53bcfb5543d662727e5d3
ShogoKitamura <shogokitamura@ShogoKitamura-no-MacBook-Air.local>
2018-09-09 21:14:46 +0900

    Initial Commit

---
+++ firebasehogeeeeeeeee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebasehogeeeeeeeee
+//
+//  Created by ShogoKitamura on 2018/09/09.
+//  Copyright © 2018年 clapyourhouse labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
